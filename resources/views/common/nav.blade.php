74510bf1b8c083c805140d1750d09a8cc7ca302c
Rodel Amancio <rodel.amancio@spacegroup.co.uk>
2018-01-09 14:41:19 +0000

    added vuforia web api

---
+++ resources/views/common/nav.blade.php
@@ -0,0 +1,13 @@
+<div class="content">
+    <div class="title m-b-md">
+        Vuforia
+    </div>
+
+    <div class="links">
+        <a href="{{ route('upload') }}">Upload</a>
+        <a href="{{ route('getList') }}">Get Data</a>
+        <a href="http://www.spacegroup.co.uk">Space Group</a>
+        <a href="http://www.spacegroup.co.uk/news">Space Group News</a>
+        <a href="https://github.com/rodelmarkamancio/Vuforia-Web-API">GitHub</a>
+    </div>
+</div>
